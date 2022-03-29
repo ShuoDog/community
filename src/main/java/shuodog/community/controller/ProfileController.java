0d72c91194da08e422f9b3b057b1b986d69d5e28
ShuoDog <1255892712@qq.com>
2022-03-29 21:44:57 +0800

    添加拦截器验证

--- src/main/java/shuodog/community/controller/ProfileController.java
+++ src/main/java/shuodog/community/controller/ProfileController.java
@@ -11,7 +11,6 @@ import shuodog.community.dto.PaginationDto;
 import shuodog.community.mapper.UserMapper;
 import shuodog.community.model.User;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -30,20 +29,7 @@ public class ProfileController {
                           HttpServletRequest request,
                           Model model) {
 
-        User user=null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+        User user=(User) request.getSession().getAttribute("user");
 
         if(user==null){
             return "redirect:/";
