9e837cc8846ccc9bb3745a9a2eb198da87896de2
ShuoDog <1255892712@qq.com>
2022-03-30 11:06:04 +0800

    完善登录功能

---
+++ src/main/java/shuodog/community/Service/UserService.java
@@ -0,0 +1,34 @@
+package shuodog.community.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import shuodog.community.mapper.UserMapper;
+import shuodog.community.model.User;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public void createOrUpdate(User user)
+    {
+        User userData = userMapper.findByAccountId(user.getAccountId());
+        if (userData==null)
+        {
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtModified());
+            userMapper.insert(user);
+            System.out.println("创建成功");
+        }
+        else
+        {
+            userData.setGmtModified(System.currentTimeMillis());
+            userData.setName(user.getName());
+            userData.setAvatarUrl(user.getAvatarUrl());
+            userData.setToken(user.getToken());
+            userMapper.update(userData);
+            System.out.println("修改成功");
+        }
+    }
+}
--- src/main/java/shuodog/community/mapper/UserMapper.java
+++ src/main/java/shuodog/community/mapper/UserMapper.java
@@ -1,9 +1,6 @@
 package shuodog.community.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import shuodog.community.model.User;
 
 @Mapper
@@ -17,4 +14,10 @@ public interface UserMapper {
 
     @Select("select * from user where id = #{id}")
     User findByID(@Param("id") Integer id);
+
+    @Select("select * from user where id = #{accountId}")
+    User findByAccountId(@Param("accountId") String accountId);
+
+    @Update("update user set name=#{name},avatar_url=#{avatarUrl},token = #{token},gmt_modified=#{gmtModified} where id = #{id}")
+    void update(User userData);
 }
